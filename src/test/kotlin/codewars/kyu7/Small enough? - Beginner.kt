97a5ce8157999fa7059d46038cb68d9f5e1154a3
Hyune-c <choi8608@gmail.com>
2023-02-19 01:34:18 +0900

    ✨ Small enough? - Beginner

---
+++ src/test/kotlin/codewars/kyu7/Small enough? - Beginner.kt
@@ -0,0 +1,18 @@
+package codewars.kyu7
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class `Small enough? - Beginner` {
+    @Test
+    fun `Basic Tests`() {
+        assertEquals(true, smallEnough(intArrayOf(66, 101), 200))
+        assertEquals(false, smallEnough(intArrayOf(78, 117, 110, 99, 104, 117, 107, 115), 100))
+        assertEquals(true, smallEnough(intArrayOf(101, 45, 75, 105, 99, 107), 107))
+        assertEquals(true, smallEnough(intArrayOf(80, 117, 115, 104, 45, 85, 112, 115), 120))
+    }
+}
+
+fun smallEnough(a: IntArray, limit: Int): Boolean {
+    return a.none { it > limit }
+}
